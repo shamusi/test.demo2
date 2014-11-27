d0255f072f6dd423247d58eace390dde3a723f47
sam <shamusi@op.pl>
2014-11-27 22:54:45 +0100

    Revert "Revert "change 2""
    This reverts commit 7b3e3f511ecd57ab985af540bc3db405af91f1bb.

--- test.demo2/Form1.vb
+++ test.demo2/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Console.WriteLine("i am sam")
+        Console.ReadLine()
+    End Sub
 End Class
